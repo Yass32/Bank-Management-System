0370a60dee81295a3b76a6ff836521b0126166a7
Yass32 <yasserzoaka@gmail.com>
2024-01-02 16:51:10 +0300

    Login class update

--- src/BankManagementSystem/Login.java
+++ src/BankManagementSystem/Login.java
@@ -14,18 +14,33 @@ public class Login extends JFrame{
         setLayout(null);
 
         //Image icon class loads an image file named "logo.jpg" from a directory named "icons." The ClassLoader is a mechanism in Java for dynamically loading classes and resources
-        ImageIcon i1 = new ImageIcon(String.valueOf(ClassLoader.getSystemResource("icons/logo.jpg")));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+
         //Get the Image object from the previously created ImageIcon(i1) then scaling the image to a size of 100x100 pixels using getScaledInstance
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+
         //Creating a new ImageIcon from the scaled Image object (i2)
         ImageIcon i3 = new ImageIcon(i2);
+
         //label will display the scaled image(i3)
         JLabel label = new JLabel(i3);
+
         //Setting the position(x, y) and size(width, height)of the JLabel
         label.setBounds(70, 10, 100, 100);
+
         //method used to add JLabel component to the JFrame container
         add(label);
 
+        //Create welcome text label
+        JLabel text = new JLabel("Welcome to ATM");
+
+        //Set the position of text close to the logo(label)
+        text.setBounds(200, 40, 100, 40);
+        //display text
+        add(text);
+
+        //Get content pane for this frame and set background color
+        getContentPane().setBackground(Color.WHITE);
         //Set window size
         setSize(800, 480);
         //Set window visibly to true
