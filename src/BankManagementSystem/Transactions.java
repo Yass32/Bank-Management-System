5ab5454da9bed49a85ffa56e46e1aa406b3f02a9
Yass32 <yasserzoaka@gmail.com>
2024-02-04 20:25:09 +0300

    Main Transactions Feature

--- src/BankManagementSystem/Transactions.java
+++ src/BankManagementSystem/Transactions.java
@@ -19,6 +19,38 @@ public class Transactions extends JFrame{
         add(backgroundImage);
 
         JLabel text = new JLabel("Please select your Transaction");
+        text.setBounds(220, 300, 700, 35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        backgroundImage.add(text);
+
+        JButton deposit = new JButton("Deposit");
+        deposit.setBounds(160, 415, 150, 30);
+        backgroundImage.add(deposit);
+
+        JButton fastCash = new JButton("Fast Cash");
+        fastCash.setBounds(160, 450, 150, 30);
+        backgroundImage.add(fastCash);
+
+        JButton pinChange = new JButton("Pin Change");
+        pinChange.setBounds(160, 485, 150, 30);
+        backgroundImage.add(pinChange);
+
+        JButton cashWithdrawal = new JButton("Cash Withdrawal");
+        cashWithdrawal.setBounds(362, 415, 150, 30);
+        backgroundImage.add(cashWithdrawal);
+
+        JButton miniStatement = new JButton("Mini Statement");
+        miniStatement.setBounds(362, 450, 150, 30);
+        backgroundImage.add(miniStatement);
+
+        JButton balanceEnquiry = new JButton("Balance Enquiry");
+        balanceEnquiry.setBounds(362, 485, 150, 30);
+        backgroundImage.add(balanceEnquiry);
+
+        JButton exit = new JButton("Exit");
+        exit.setBounds(362, 520, 150, 30);
+        backgroundImage.add(exit);
 
 
         //Get content pane for this frame and set background color
@@ -29,6 +61,7 @@ public class Transactions extends JFrame{
         setVisible(true);
         //Set window position to middle of the screen
         setLocation(300, 0);
+        //setUndecorated(true);
     }
 
     public static void main(String[] args) {
